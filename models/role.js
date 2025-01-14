8295cc573e810888e38f9a96820f8ae601bfbdcd
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-14 12:02:23 +0900

    delete old role model

--- models/role.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const roleSchema = mongoose.Schema({
-  name: String,
-});
-
-const Role = mongoose.model("Role", roleSchema);
-
-module.exports = Role;
