a8071cc168fe3a8b145255e2af47cdfb93856989
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-27 14:13:40 +0900

    task model

---
+++ models/task.model.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const Task = mongoose.model(
+  "Task",
+  new mongoose.Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      description: {
+        type: String,
+        trim: true,
+      },
+      dueDate: {
+        type: Date,
+        required: true,
+      },
+      priority: {
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "medium",
+      },
+      status: {
+        type: String,
+        enum: ["todo", "in_progress", "completed"],
+        default: "todo",
+      },
+      assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      // projectId: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   ref: "Project",
+      // },
+      labels: [
+        {
+          type: String,
+          trim: true,
+        },
+      ],
+      attachments: [
+        {
+          name: String,
+          url: String,
+          type: String,
+        },
+      ],
+      createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+module.exports = Task;
