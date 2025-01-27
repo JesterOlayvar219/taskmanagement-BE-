b56ddc9d99ace4a8cbba9a2359899967bc35bc43
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-27 14:12:30 +0900

    project model

--- models/Project.js
+++ models/Project.js
@@ -1,22 +1,32 @@
 const { default: mongoose } = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: String,
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  description: String,
-  members: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
- 
+);
+
+// const Project = mongoose.model("Project", projectSchema);
+
+// module.exports = Project;
+
+export default Project = mongoose.model("Project", projectSchema);
