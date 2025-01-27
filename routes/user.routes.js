ae01b98f273a42d9eec638adbe33b3203af16f97
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-27 14:14:15 +0900

    user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,7 @@ const controller = require("../controllers/user.controller");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    res.header("Access-Contol-Allow-Headers", "Origin, Content-Type, Accept");
-    console.log("resData:", res.header);
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     next();
   });
 
