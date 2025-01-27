c8dc00fac9b1c37dc250a44f11358fc878bb6f8c
JesterOlayvar219 <jester.olayvar.x@gmail.com>
2025-01-27 14:11:41 +0900

    comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,20 +1,27 @@
 const { default: mongoose } = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  taskId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Task',
-    required: true
+const commentSchema = new mongoose.Schema(
+  {
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
